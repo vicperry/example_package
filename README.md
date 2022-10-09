9d8ff39c73cb2af71157534736400d4f852f71f1
vicperry <vic.hugx@gmail.com>
2022-10-09 11:22:23 -0300

    Package setted.

--- README.md
+++ README.md
@@ -1,27 +1,27 @@
 # package_name
 
 Description. 
-The package package_name is used to:
+The package hello_world is used to:
 	- 
 	-
 
 ## Installation
 
-Use the package manager [pip](https://pip.pypa.io/en/stable/) to install package_name
+Use the package manager [pip](https://pip.pypa.io/en/stable/) to install hello_world
 
 ```bash
-pip install package_name
+pip install hello_world
 ```
 
 ## Usage
 
 ```python
-from package_name import file1_name
+from hello_world import file1_name
 file1_name.my_function()
 ```
 
 ## Author
-My_name
+Victor
 
 ## License
 [MIT](https://choosealicense.com/licenses/mit/)
--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 setup(
-    name="package_name",
+    name="hello_world",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    author="vicperry",
+    author_email="vic.hugx@gmail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
-    url="my_github_repository_project_link"
+    url="https://github.com/vicperry/example_package",
     packages=find_packages(),
     install_requires=requirements,
     python_requires='>=3.8',
